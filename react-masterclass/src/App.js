7f89fc9b6c2ffbb6a0098ed0c4abd12b1b3ed314
syjeong <sparrowr2723@gmail.com>
2025-06-06 15:06:15 +0900

    06/06 리액트 마스터 클래스 파트2

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -1,37 +1,50 @@
-import styled from "styled-components"
+import styled, {keyframes} from "styled-components"
 
-const Father = styled.div`
+const Title = styled.h1`
+    color: ${(props) => props.theme.textColor};
+`;
+
+const Wrapper = styled.div`
     display: flex;
+    height: 100vh;
+    width: 100vw;
+    justify-content: center;
+    align-items: center;
+    background-color: ${props => props.theme.backgroundColor};
 `;
+
+const rotationAnimation = keyframes`
+    0% {transform: rotate(0deg); border-radius: 0px;}
+    50% {border-radius: 100px;}
+    100% {transform: rotate(360deg); border-radius: 0px;}
+`;
+
+const Emoji =  styled.span`
+    font-size: 40px; 
+`;
+
 const Box = styled.div`
-    background-color: ${props => props.bgColor}; 
-    width: 100px; 
-    height: 100px;
+    height: 200px;
+    width: 200px;
+    background-color: ${(prop) => prop.bgColor};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    animation: ${rotationAnimation} 1s linear infinite;
+    ${Emoji}:hover {
+        font-size: 98px;
+    }
 `;
-const Circle = styled(Box)`
-    border-radius: 50%;
-`;
-const Btn = styled.button`
-    color:white;
-    background-color: tomato;
-    border: 0;
-    border-radius: 15px;
-`;
-const Link = styled(Btn)`
-`;
-const Input = styled.input.attrs({ required: true, minLength: 10 })`
-    background-color: tomato;
-`
 
 function App() {
     return (
-        <Father as={"header"}>
-            <Input />
-            <Input />
-            <Input />
-            <Input />
-            <Input />
-        </Father>
+        <Wrapper>
+            <Title>Title</Title>
+            <Box bgColor="tomato">
+                <Emoji>😊</Emoji>
+            </Box>
+            <Emoji>😊</Emoji>
+        </Wrapper>
     );
 }
 
