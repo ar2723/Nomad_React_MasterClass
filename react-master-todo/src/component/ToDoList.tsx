4354a4170651800fffdf046ee7d61bd45f898db3
정세영 <ar2723@naver.com>
2025-07-23 14:12:52 +0900

    2025.07.23 노마드 리액트 / todoList Validation

--- react-master-todo/src/component/ToDoList.tsx
+++ react-master-todo/src/component/ToDoList.tsx
@@ -1,19 +1,56 @@
-import {useState} from "react";
+import {useForm} from "react-hook-form";
+import {IFormData} from "../interface/todoInterface";
+import {Helmet} from "react-helmet-async";
 
-const ToDoList = () => {
-    const [todo, setTodos] = useState("");
-    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-        const {currentTarget: { value }} = event;
-        setTodos(value)
-    };
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log(todo);
+function ToDoList() {
+    const { register, watch, handleSubmit, formState: { errors }} = useForm<IFormData>({
+        defaultValues: {
+            email: "@naver.com"
+        }
+    });
+    const onValid = (data:IFormData) => {
+        console.log(data);
     }
     return (
         <>
-            <form onSubmit={onSubmit}>
-                <input onChange={onChange} value={todo} placeholder={"Write a to do"}/>
+            <Helmet><title>TodoList</title></Helmet>
+            <form style={{ display: "flex", flexDirection: "column"}} onSubmit={handleSubmit(onValid)}>
+                <input {...register("email", {
+                    required: "Email is required",
+                    pattern: {
+                        value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+                        message: "Only Naver.com emails is allowed"
+                    }
+                })} placeholder={"email"}/>
+                <span>{errors?.email?.message}</span>
+
+                <input {...register("firstName", { required: true })}
+                       placeholder={"firstName"}/>
+                <span>{errors?.firstName?.message}</span>
+
+                <input {...register("lastName", { required: true })}
+                       placeholder={"lastName"}/>
+                <span>{errors?.lastName?.message}</span>
+
+                <input {...register("username", { required: true, minLength: 10 })}
+                       placeholder={"username"}/>
+                <span>{errors?.username?.message}</span>
+
+                <input {...register("password", {
+                    required: "Password is required",
+                    minLength: {
+                        value: 5,
+                        message: "Password must be at least 5 characters long"
+                    }}
+                )} placeholder={"password"}/>
+                <span>{errors?.password?.message}</span>
+
+                <input {...register("CheckingPassword", {
+                    required: "CheckingPassword is required"
+                    , minLength: 5
+                })} placeholder={"CheckingPassword"}/>
+                <span>{errors?.CheckingPassword?.message}</span>
+
                 <button>Add</button>
             </form>
         </>
---
+++ react-master-todo/src/interface/todoInterface.ts
@@ -0,0 +1,13 @@
+export interface IFormData {
+    errors: {
+        email: {
+            message: string;
+        };
+    };
+    firstName: string;
+    username: string;
+    lastName: string;
+    email: string;
+    password: string;
+    CheckingPassword: string;
+}
