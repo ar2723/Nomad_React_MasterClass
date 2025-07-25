92c0db6d1fb6b6cae18f00b0dee58b8b94246a64
정세영 <ar2723@naver.com>
2025-07-25 17:54:21 +0900

    2025.07.25 노마드 리액트 / trello / dnd 초기 세팅

--- react-master-trello/src/App.tsx
+++ react-master-trello/src/App.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
-import {GlobalStyle} from "./theme/GlobalStyle";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd"
 
 function App() {
+    const onDragEnd = () => {
 
+    }
     return (
-        <div>
-          <GlobalStyle/>
-
-        </div>
+        <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId={"one"}>
+                {(provided, snapshot) =>
+                    <ul {...provided.droppableProps} ref={provided.innerRef}>
+                        <Draggable draggableId={"first"} index={0}>
+                            {(provided) =>
+                                <li ref={provided.innerRef}
+                                    {...provided.dragHandleProps}
+                                    {...provided.draggableProps}>One
+                                </li>
+                            }
+                        </Draggable>
+                        <Draggable draggableId={"second"} index={1}>
+                            {(provided) =>
+                                <li ref={provided.innerRef}
+                                    {...provided.dragHandleProps}
+                                    {...provided.draggableProps}>Two
+                                </li>
+                            }
+                        </Draggable>
+                    </ul>
+                }
+            </Droppable>
+        </DragDropContext>
     );
 }
 
