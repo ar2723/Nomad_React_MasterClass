ffbe97d80adb3b7afee6374eef287c022ed72083
정세영 <ar2723@naver.com>
2025-07-28 17:37:06 +0900

    2025.07.25 노마드 리액트 / trello / 싱글 보드 드래그 완

--- react-master-trello/src/App.tsx
+++ react-master-trello/src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import {DragDropContext, Droppable, Draggable, DropResult} from "react-beautiful-dnd"
+import {DragDropContext, Droppable, DropResult} from "react-beautiful-dnd"
 import {GlobalStyle} from "./theme/GlobalStyle";
-import {Board, Boards, Card, Wrapper} from "./component/layout";
+import {Board, Boards, Wrapper} from "./component/layout";
 import {useRecoilState} from "recoil";
 import {toDoState} from "./interface/toDoState";
-
-
-
+import DraggabbleCard from "./component/DraggabbleCard";
 
 function App() {
     const [toDos, setToDos] = useRecoilState(toDoState)
@@ -22,6 +20,7 @@ function App() {
         })
     }
     return (
+        // 일반적으로 부모의 Component가 변경되면 자식 Component가 모두 재렌더링되어버린다.
         <>
             <GlobalStyle />
             <DragDropContext onDragEnd={onDragEnd}>
@@ -30,15 +29,11 @@ function App() {
                         <Droppable droppableId={"one"}>
                             {(provided) =>
                                 <Board ref={provided.innerRef} {...provided.droppableProps}>
-                                    {toDos.map(((todo, index) => (
-                                        <Draggable key={todo} draggableId={todo} index={index}>
-                                            {(provided) =>
-                                                <Card ref={provided.innerRef} {...provided.dragHandleProps} {...provided.draggableProps}>
-                                                    {todo}
-                                                </Card>
-                                            }
-                                        </Draggable>
-                                    )))}
+                                    {toDos.map(
+                                        ((todo, index) =>
+                                            <DraggabbleCard key={todo} todo={todo} index={index}/>
+                                        ))
+                                    }
                                     {provided.placeholder}
                                 </Board>
                             }
---
+++ react-master-trello/src/component/DraggabbleCard.tsx
@@ -0,0 +1,26 @@
+import {Card} from "./layout";
+import {Draggable} from "react-beautiful-dnd";
+import React from "react";
+
+interface IDraggableCardProps {
+    todo: string,
+    index: number,
+}
+
+function DraggableCard({todo, index}: IDraggableCardProps) {
+    console.log("this component is rendered");
+    return (
+        // 여기서 key와 draggableId를 반드시 일치시켜줘야 함 - 왜인지는 모르겠으나 다르면 버그가 발생
+        <Draggable key={todo} draggableId={todo} index={index}>
+            {(provided) =>
+                <Card ref={provided.innerRef}
+                      {...provided.dragHandleProps}
+                      {...provided.draggableProps}
+                >{todo}
+                </Card>
+            }
+        </Draggable>
+    );
+}
+// prop이 바뀌는 요소에 대해서만 다시 렌더링 되도록 지정 - 최적화 목적
+export default React.memo(DraggableCard);
